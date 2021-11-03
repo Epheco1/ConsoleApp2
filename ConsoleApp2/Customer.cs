6942c33568ff81929b5c7796ff0f6d1a3d1cef83
Epheco1 <Epheco1@DESKTOP-450UJA3>
2021-11-03 11:40:34 +0100

    Add project files.

---
+++ ConsoleApp2/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Customer
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public void Login() 
+        {
+            Console.WriteLine("Pls Enter Your PhoneNumber");
+            PhoneNumber = Console.ReadLine();
+
+            if (PhoneNumber == "08011223344")
+            {
+                Console.WriteLine("Welcome");
+            }
+        }
+
+        public void Deposit() 
+        {
+            double initialBalance, newBalance, amount;
+            initialBalance = 5000;
+            Console.WriteLine("How much would you like to deposit?");
+            amount = double.Parse(Console.ReadLine());
+            newBalance = initialBalance + amount;
+            
+            Console.WriteLine(" Your new Balance is : " + newBalance);
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class MobileBanking
+    {
+
+        static void Main(string[] args)
+        {
+            Customer newCustomer = new Customer();
+
+            newCustomer.Login();
+            newCustomer.Deposit();
+
+        }
+    }
+}
